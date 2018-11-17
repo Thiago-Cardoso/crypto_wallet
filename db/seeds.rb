b96b200595fca00429951bdf7ed4c7a3ccc62e6b
Thiago-Cardoso <trovador182@gmail.com>
2018-11-17 17:44:55 -0200

    create CRUD Mining of Type

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Create coins..."
+# Use o rails dev:setup (lib/tasks/dev.rake) 
 
-spinner = TTY::Spinner.new("[:spinner] Registering Coins...")
-spinner.auto_spin
-
-coins = [ 
-    {
-        description: "Bitcoin",
-        acronym: "BTC",
-        url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
-    },
-
-    {
-        description: "Ethereum",
-        acronym: "ETH",
-        url_image: "https://i.warosu.org/data/biz/img/0027/98/1500220164928.png"
-    },
-
-    {
-        description: "Dash",
-        acronym: "DASH",
-        url_image: "http://moondash.co.in/coin/logo.png"
-    }
-]
-
-coins.each do |coin|
-    sleep(1)
-    Coin.find_or_create_by!(coin)
-end
-
-spinner.success("(Done!)")
